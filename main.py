a1c24b1876c71ec527765a1fd005da4bc03f4885
yashah09 <shahyash99@gmail.com>
2025-04-18 10:37:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -65,4 +65,4 @@ def upload():
     return jsonify({'message': 'Uploaded successfully', 'link': file_link})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
